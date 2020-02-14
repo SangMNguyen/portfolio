b938a4726b03098628d02a639ed72a0ebe619f12
Sang Nguyen <sangnguyenpublic@gmail.com>
2020-02-13 21:48:54 -0500

    preparation for new routing

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 export default class Project extends Component {
     render() {
         return (
-            <div className="projects">Projects</div>
+            <div className={`projects ${this.props.inView ? 'current' : ''}`}>Projects</div>
         );
     }
 }
