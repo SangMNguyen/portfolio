fa21041d8bcc67f919b09bc3803bd0be4a194fdd
Sang Nguyen <sangnguyenpublic@gmail.com>
2020-02-13 21:48:40 -0500

    updated home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 export default class Home extends Component {
     render() {
         return (
-            <div className="main">
-                <h1>S A N G &nbsp; M. &nbsp; N G U Y E N</h1>
+            <div className={`main ${this.props.inView ? 'current' : ''}`}>
+                <h1>SANG M. NGUYEN</h1>
                 <div className="links">
                     <Link to="/bio" title="View Biography">Biography</Link>
                     <Link to="/projects" title="View Projects">Projects</Link>
