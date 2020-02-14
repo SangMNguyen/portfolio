68a23596524b93630c7eab088c402fa43799fc46
Sang Nguyen <sangnguyenpublic@gmail.com>
2020-02-13 21:47:41 -0500

    updated home icon

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -12,7 +12,11 @@ export default class Logo extends Component {
 
     render() {
         return (
-            <div className={`logo`} style={this.state.style}>SN</div>
+            <div className={`logo`} style={{color: this.state.color}}><span>SN</span>
+                <svg height="75" width="75" viewBox="0 0 75 75" style={{position: 'absolute', left: '14px', top: '-8px'}}>
+                    <circle id='logoRing' cx="37.5" cy="37.5" r="35"/>
+                </svg>
+            </div>
         );
     }
 }
