255d274d93fe7cc1095268a68687371bf4c35db7
Sang Nguyen <sangnguyenpublic@gmail.com>
2020-02-13 21:47:30 -0500

    inputfield component for contact page

---
+++ src/components/InputField.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+import Checkmark from '../assets/checkmark.svg';
+
+export default class InputField extends Component {
+
+    render() {
+        return (
+            <div className="inputField">
+                <label htmlFor={this.props.name}>{this.props.title}{this.props.filled && <img src={Checkmark} title={'Completed!'} alt={''}/>}</label>
+                <input 
+                    type={this.props.name === 'email' ? 'email' : 'text'} 
+                    name={this.props.name} 
+                    placeholder={this.props.placeholder} 
+                    onChange={this.props.onChange}
+                    className={this.props.filled ? 'filled' : ''}
+                    required
+                />
+            </div>
+        );
+    }
+}
