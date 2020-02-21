2e8953051a81dfd1ff42b99a8850f898a1a09920
Sang Nguyen <sangnguyenpublic@gmail.com>
2020-02-20 22:47:21 -0500

    Adjusted home layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,15 +2,18 @@ import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 
 export default class Home extends Component {
+
     render() {
         return (
             <div className={`main ${this.props.inView ? 'current' : ''}`}>
+                <h3 id="typing">I am</h3>
                 <h1>SANG M. NGUYEN</h1>
                 <div className="links">
                     <Link to="/bio" title="View Biography">Biography</Link>
                     <Link to="/projects" title="View Projects">Projects</Link>
                     <Link to="/contact" title="View Contact Info">Contact</Link>
-                </div>                
+                </div>
+                <p>Designed &amp; Coded by Sang Nguyen | Pictures by Bryant Lee</p>
             </div>
         );
     }
