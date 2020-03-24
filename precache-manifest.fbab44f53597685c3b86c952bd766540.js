60790dd66d4bac524f0e0482b7570b4f02563120
Sang Nguyen <sangnguyenpublic@gmail.com>
2020-03-24 15:02:01 -0400

    Updates

---
+++ precache-manifest.fbab44f53597685c3b86c952bd766540.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22d1386fb696cd8517fbaaa20cd7dcf8",
+    "url": "/website2.0/index.html"
+  },
+  {
+    "revision": "e0e1dad8a5ef4413a8ee",
+    "url": "/website2.0/static/css/2.83ace7dd.chunk.css"
+  },
+  {
+    "revision": "0710bfbd2cb7b414ba85",
+    "url": "/website2.0/static/css/main.643fec3e.chunk.css"
+  },
+  {
+    "revision": "e0e1dad8a5ef4413a8ee",
+    "url": "/website2.0/static/js/2.caadcccd.chunk.js"
+  },
+  {
+    "revision": "0710bfbd2cb7b414ba85",
+    "url": "/website2.0/static/js/main.f5008982.chunk.js"
+  },
+  {
+    "revision": "5acd280f5dad4f094453",
+    "url": "/website2.0/static/js/runtime-main.05c2c4a5.js"
+  },
+  {
+    "revision": "39d59a295c161309ac6d6f574ba7774e",
+    "url": "/website2.0/static/media/Resume.39d59a29.pdf"
+  },
+  {
+    "revision": "6be5f861cb95575ef0d2d8c89b3c08f2",
+    "url": "/website2.0/static/media/arrowhead.6be5f861.svg"
+  },
+  {
+    "revision": "966df0c0e962865dfbb0767139c374d1",
+    "url": "/website2.0/static/media/bio1.966df0c0.jpg"
+  },
+  {
+    "revision": "4ca0cb969a37b8a212d8cb5965ddf7da",
+    "url": "/website2.0/static/media/bio2.4ca0cb96.jpg"
+  },
+  {
+    "revision": "61c1ab1c8b8471eba6a651c52b95a5a6",
+    "url": "/website2.0/static/media/bio3.61c1ab1c.jpg"
+  },
+  {
+    "revision": "20541dd81673628e1918c94b2341fcab",
+    "url": "/website2.0/static/media/bio4.20541dd8.jpg"
+  },
+  {
+    "revision": "3db97366b99989c2e007d866940a7542",
+    "url": "/website2.0/static/media/checkmark.3db97366.svg"
+  },
+  {
+    "revision": "bb2f8d988dc54480336bbb5b9b00b481",
+    "url": "/website2.0/static/media/discordback.bb2f8d98.png"
+  },
+  {
+    "revision": "6bf7f4df112e7218e080bb66f50f932d",
+    "url": "/website2.0/static/media/discordimg.6bf7f4df.png"
+  },
+  {
+    "revision": "6288edccb09a990fe05d175f8d89081c",
+    "url": "/website2.0/static/media/ggback.6288edcc.png"
+  },
+  {
+    "revision": "3746a77c5b8ec8f597d9388564ce6fda",
+    "url": "/website2.0/static/media/ggimg.3746a77c.png"
+  },
+  {
+    "revision": "0facaae97183b8fede52099930aefd8d",
+    "url": "/website2.0/static/media/helveticaneue.0facaae9.ttf"
+  },
+  {
+    "revision": "a2b6fe214181dcf62b35fbfca402dae1",
+    "url": "/website2.0/static/media/pong.a2b6fe21.gif"
+  },
+  {
+    "revision": "60a28b45e4102130951c3aabbe160923",
+    "url": "/website2.0/static/media/t4tback.60a28b45.png"
+  },
+  {
+    "revision": "32f996b6926aad7fd8731a07d54b66ae",
+    "url": "/website2.0/static/media/t4timg.32f996b6.png"
+  }
+]);
