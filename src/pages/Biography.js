dbb173fd739b466b240253e8234f03c62dd763ba
Sang Nguyen <sangnguyenpublic@gmail.com>
2020-06-14 23:55:37 -0400

    finished writing out text, and updated projects to include links

--- src/pages/Biography.js
+++ src/pages/Biography.js
@@ -13,39 +13,44 @@ const content = [
         text: `My name is Sang Minh Nguyen. I am of Vietnamese descent, and a front-end
         developer based in the New Jersey area. I moved from Vietnam at a young age,
         but growing up the United States has been mostly rewarding, given the resources 
-        and opportunities that have opened up for me.\n\n In the field, I love established
-        `,
+        and opportunities that have opened up for me. I moved around a lot as a kid, but
+        I settled down in New Jersey and quite late into my academic career, decided to
+        be a programmer.`,
     },
     {
         image: Image2,
         title: 'Why Development?',
         text: `I love creating things ever since I was a kid. I'd always play with Legos and 
         loved every step of the process from designing to building. As a result, I pursued 
-        Architecture in highschool, believing it to be my passion. Although I enjoyed it, and 
-        the mathematics and concepts weren't difficult to grasp, I realized the creative aspect
-        was less than what I'd expected. Fortunately, immediately after, my attention turned 
-        to the world of programming. Doing independent research into this world had enlightened
-        me about the process of development. I quickly fell in love with in, since I've always 
-        been good with recognizing patterns, and I've felt no better place to use that until 
-        I found programming. It's also an amazing outlet to release my creativity as well as 
-        my love for solving logic puzzles.`,
+        Architecture in highschool, believing it to be my passion. Although it was a 
+        fascinating field, I found that the creativity can be limited. Thus, my attention 
+        turned to the world of programming. I quickly fell in love with it. It appealed 
+        with my ability to recognize patterns and I've felt no better place to use that. 
+        It's also an amazing outlet to release my creativity as well as my love for solving 
+        logic puzzles.`,
     },
     {
         image: Image3,
         title: 'What Else Do I Do?',
         text: `In addition to design and development, I also enjoy other hobbies and
         activities. In my sparetime, I play video games of all types, ranging from casual
-        puzzles to competitive tactical shooters. Additionally, I enjoy working out as well as hiking, often dabbling 
-        in a bit of photography, typically taking landscapes in scenic areas whenever I travel elsewhere. Furthermore, I love writing stories, 
+        puzzles to competitive tactical shooters. Additionally, I enjoy working out as well 
+        as hiking, often dabbling in a bit of photography, typically taking landscapes in 
+        scenic areas whenever I travel elsewhere. Furthermore, I love writing stories, 
         oftenly in the form of screenplays and scripts. One day, I do aspire to direct and 
-        produce those stories into actual films. I also enjoy karaoke tremendously despite not being
-        that good of a singer.`,
+        produce those stories into actual films. I also enjoy karaoke tremendously despite 
+        not being that good of a singer.`,
     },
     {
         image: Image4,
         title: 'My Aspirations for Life',
-        text: `What is the overall goal for my work? I'm not exactly someone who's pursuing this career
-        for its salary value or status. I enjoy the work that I do, but `,
+        text: `What is the overall goal for my work? I'm not exactly someone who's pursuing 
+        this career for its salary value or status. I enjoy the work that I do, but I 
+        especially enjoy seeing my projects being used as intended (or for better) after 
+        deployment. I'd like to expand my career farther then a simple front-end developer, 
+        and look into projects with other areas from Artificial Intelligence to Game Design.
+        My next projects are a bit ambitious, but it only adds to my excitement and motivation. 
+        I hope to be able to participate in more projects that help better the world.`,
     }
 ];
 
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -11,6 +11,8 @@ const Back2 = require('../assets/ggback.png');
 const Back3 = require('../assets/discordback.png');
 const Back4 = require('../assets/t4tback.png');
 
+const backgrounds = [Back1, Back2, Back3, Back4];
+
 const content = [
     {
         image: Image1,
@@ -19,45 +21,35 @@ const content = [
         structure of creating a game. I learned concepts from a tutorial that dealt with
         the basics of making a game, and used those concepts to recreate this popular game,
         Pong. The core gameplay mechanic was finished, and it is a fully functional local
-        2-player game.`
+        2-player game.`,
+        link: 'https://github.com/SangMNguyen/Pong', 
     }, {
         image: Image2,
         title: 'Project GG',
         text: `The goal of this project was to create a social media platform that focused
         on videogame fans as its primary audience. It was a collaboration between colleagues
-        and it is a complete work-in-progress which had to be temporarily shelved due to [REDACTED]`
+        and it is a complete work-in-progress which had to be temporarily shelved due to 
+        academic and work responsibilities.`,
+        link: 'https://github.com/sangmattxavier/Project-GG',
     }, {
         image: Image3,
         title: 'Discord Bot',
         text: `Discord bots are simply helpers or additional forms of entertainment for a Discord
         Server. There are many Discord bots that offer different features and the intention of
         my bot was to have a combination of these features, readily available. Currently, the
-        bot offers many recreational options like games (blackjack, rock/paper/scissors, etc.)`
+        bot offers many recreational options like games (blackjack, rock/paper/scissors, etc.)`,
+        link: 'https://github.com/SangMNguyen/DiscordBot',
     }, {
         image: Image4,
         title: 'Thought4Thought',
         text: `Thought4Thought was a startup company that offered a project for the NJIT
-        Capstone program. I was apart of this project along with 4 other students and my
-        role was 1 of the 2 Front-End Developers. My reponsibilities included implementing
-        wireframes that our UX Designer came up with, translating the wireframes to a high-
-        fidelity design, discussing a logical flow of the application, and connecting the
-        Back-End API to Front-End functions.`
+        Capstone program. My reponsibilities included implementing wireframes that our UX Designer 
+        came up with, translating the wireframes to a high-fidelity design, discussing a logical 
+        flow of the application, and connecting the Back-End API to Front-End functions.`
     }
 ];
 
 export default class Projects extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            backgrounds: [Back1, Back2, Back3, Back4],
-            links: [
-                'https://github.com/SangMNguyen/Pong', 
-                'https://github.com/sangmattxavier/Project-GG',
-                'https://github.com/SangMNguyen/DiscordBot'
-            ],
-        }
-    }
 
     render() {        
         return (
@@ -65,7 +57,7 @@ export default class Projects extends Component {
                 <div className={`projects`}>
                     <Showcase 
                         content={content}
-                        backgrounds={this.state.backgrounds}
+                        backgrounds={backgrounds}
                     />
                 </div>
             </div>
