844b32d657f673054fc3c434aea65dab442b7aab
Sang Nguyen <44916061+SangMNguyen@users.noreply.github.com>
2020-11-12 23:35:03 -0500

    functional component
    added traveling between slideshows

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -1,59 +1,56 @@
-import React, {Component} from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import Pip from './Pip';
 
 const arrow = require("../assets/arrowhead.svg");
 const githubDark = require("../assets/githubDark.png");
 
-export default class Showcase extends Component {
-    constructor(props) {
-        super(props);
+const Showcase = props => {
+    const [curIndex, setIndex] = useState(props.startValue ? props.startValue - 1 : 0);
+    const [hideView, setView] = useState(props.backgrounds ? false : null);
+    const history = useHistory();
+    const slides = props.content;
+    const backgrounds = props.backgrounds;
 
-        this.state = {
-            currentIndex: 0,
-            images: this.props.images,
-            text: this.props.text,
-            titles: this.props.titles,
-            backgrounds: this.props.backgrounds,
-            links: this.props.links,
-            hideView: this.props.backgrounds ? false : null,
-        }
-    }
-
-    nextSlide = () => {
-        if(this.state.currentIndex === this.props.content.length - 1) {
-            this.setState({currentIndex: 0})
+    const nextSlide = () => {
+        if(curIndex === slides.length - 1) {
+            if(props.nextLoc) {
+                history.push(props.nextLoc);
+            }
             return;
         }
-        this.setState({currentIndex: this.state.currentIndex + 1});
+        setIndex(curIndex + 1);
     }
 
-    prevSlide = () => {
-        if(this.state.currentIndex === 0) {
-            this.setState({currentIndex: this.props.content.length - 1})
+    const prevSlide = () => {
+        if(curIndex === 0) {
+            if(props.prevLoc) {
+                history.push(props.prevLoc);
+            }
             return;
         }
-        this.setState({currentIndex: this.state.currentIndex - 1});
+        setIndex(curIndex - 1);
     }
 
-    setSlide = (num) => {
-        if(this.state.currentIndex === num) {
+    const setSlide = num => {
+        if(curIndex === num) {
             return;
         }
-        this.setState({currentIndex: num});
+        setIndex(num);
     }
 
-    handleKey = (e) => {
+    const handleKey = e => {
         let key = e.key.split('Arrow');
         if(key.length !== 2) {
             return;
         }
         switch(key[1]) {
-            case 'Right':
-                this.nextSlide();
+            case "Right":
+                nextSlide();
                 break;
             
-            case 'Left':
-                this.prevSlide();
+            case "Left":
+                prevSlide();
                 break;
             
             default:
@@ -61,43 +58,36 @@ export default class Showcase extends Component {
         }
     }
 
-    showBackground = () => {
-        this.setState({ hideView: !this.state.hideView})
-    }
+    useEffect(() => {
+        document.addEventListener("keydown", handleKey, false);
+        return () => document.removeEventListener("keydown", handleKey, false);
+    }, [handleKey]);
 
-    componentWillMount() {
-        document.addEventListener("keydown", this.handleKey, false);
-    }
-
-    componentWillUnmount() {
-        document.removeEventListener("keydown", this.handleKey, false);
-    }
-
-    render() {
-        const slides = this.props.content;
         
-
-        return (
-            <div className={`${this.props.backgrounds ? 'showcaseplus' : 'showcase'}`}>
-                {this.props.backgrounds && this.state.backgrounds.map((item, index) => 
-                    <img className={`background ${this.state.currentIndex === index ? 'active' : ''} ${this.state.hideView ? 'unblur' : ''}`} src={item} alt={''} />
-                )}
-                <div className={`view ${this.state.hideView ? 'hidden' : ''}`}>
-                    {slides.map((item, index) => 
-                        <div className={`slide ${this.state.currentIndex === index ? 'active' :
-                        this.state.currentIndex >= index ? 'next' :
-                        this.state.currentIndex <= index ? 'prev' : ''}`} key={index}>
-                            {item.link ? <a href={item.link} title={"Go to Repository"} target="_blank" rel="noopener noreferrer" className="image">
-                                <img src={item.image} alt={`View ${index}`}/>
-                            </a> : 
-                            <div className="image" title={this.props.backgrounds ? "No Github link attached." : ""}>
-                                <img src={item.image} alt={`View ${index}`}/>
-                            </div>}
-                            <div className="text">
-                                <h2 className="title">{item.title}</h2>
-                                {item.text}
-                                {item.link && 
-                                <a href={item.link} 
+    return (
+        <div className={`${backgrounds ? 'showcaseplus' : 'showcase'}`}>
+            {backgrounds && backgrounds.map((item, index) => 
+                <img className={`background ${curIndex === index ? 'active' : ''} ${hideView ? 'unblur' : ''}`} src={item} alt={''} />
+            )}
+            <div className={`view ${hideView ? 'hidden' : ''}`}>
+                {slides.map((item, index) => 
+                    <div className={`slide ${curIndex === index ? 'active' :
+                        curIndex >= index ? 'next' :
+                        curIndex <= index ? 'prev' : ''}`} 
+                        key={"slide" + index}
+                    >
+                        {item.link ? <a href={item.link} title={"Go to Repository"} target="_blank" rel="noopener noreferrer" className="image">
+                            <img src={item.image} key={"github" + index} alt={`View ${index}`}/>
+                        </a> : 
+                        <div className="image" title={backgrounds ? "No Github link attached." : ""}>
+                                <img src={item.image} key={"github" + index} alt={`View ${index}`}/>
+                        </div>}
+                        <div className="text">
+                            <h2 className="title">{item.title}</h2>
+                            {item.text}
+                            {item.link && 
+                            <a 
+                                href={item.link} 
                                 title={"Go to " + item.title} 
                                 target="_blank" 
                                 rel="noopener noreferrer" 
@@ -110,40 +100,57 @@ export default class Showcase extends Component {
                                     borderRadius: "10px", 
                                     position: "absolute", 
                                     bottom: "3.15rem", 
-                                    right: "3.15rem"}}>
-                                    <img src={githubDark} 
+                                    right: "3.15rem"
+                                }}
+                            >
+                                <img 
+                                    src={githubDark} 
                                     alt="Github Link" 
-                                    title="Go to Repository"/>
-                                </a>}
-                            </div>
+                                    title="Go to Repository"
+                                />
+                            </a>}
                         </div>
+                    </div>
+                )}
+            </div>
+            <div className={`slideNav`}>
+                <div id={'leftArrow'}>
+                    <img 
+                        src={arrow}
+                        className={curIndex === 0 && !props.prevLoc ? "poof" : "appear"} 
+                        onClick={prevSlide} 
+                        title="Go Back" 
+                        alt={''}
+                    />
+                    {props.prevLoc && <Link className={`leftLink ${curIndex === 0 ? "shown" : ""}`} to={props.prevLoc}>{props.prevLocName}</Link>}
+                </div>
+                <div className="pipBox">
+                    {slides.map((item, index) => 
+                        <Pip key={index} active={curIndex === index} title={"Slide " + (index + 1)} onClick={() => setSlide(index)}/>
                     )}
                 </div>
-                <div className="slideNav">
-                    <img src={arrow} id={'leftArrow'} 
-                    className={this.state.currentIndex === 0 ? "poof" : "appear"} 
-                    onClick={this.prevSlide} 
-                    title="Go Back" 
-                    alt={''}/>
-                    <div className="pipBox">
-                        {slides.map((item, index) => 
-                            <Pip key={index} active={this.state.currentIndex === index} title={"Slide " + (index + 1)} onClick={() => this.setSlide(index)}/>
-                        )}
-                    </div>
-                    <img src={arrow} id={'rightArrow'} 
-                    className={this.state.currentIndex === this.props.content.length - 1 ? "poof" : "appear"} 
-                    onClick={this.nextSlide} 
-                    title="Next Slide" alt={''}/>
+                <div id={'rightArrow'}>
+                    <img 
+                        src={arrow} 
+                        className={curIndex === slides.length - 1 && !props.nextLoc ? "poof" : "appear"} 
+                        onClick={nextSlide} 
+                        title="Next Slide" 
+                        alt={''}
+                    />
+                    {props.nextLoc && <Link className={`rightLink ${curIndex === slides.length - 1 ? "shown" : ""} ${props.nextAdj ? "adjust" : ""}`} to={props.nextLoc}>{props.nextLocName}</Link>}
                 </div>
-                {this.props.backgrounds &&
-                    <div className="hideView" 
-                    title="Hover to See Background" 
-                    onMouseEnter={this.showBackground} 
-                    onMouseLeave={this.showBackground}>
-                        Hover Here
-                    </div>
-                }
             </div>
-        );
-    }
+            {props.backgrounds &&
+                <div 
+                    className="hideView" 
+                    title="Hover to See Background" 
+                    onMouseEnter={() => setView(true)} 
+                    onMouseLeave={() => setView(false)}
+                >
+                    Hover Here
+                </div>
+            }
+        </div>
+    );
 }
+export default Showcase;
