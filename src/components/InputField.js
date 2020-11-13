2bfdf27e3a96a5e2f1f751cd54557e33cde33ee0
Sang Nguyen <44916061+SangMNguyen@users.noreply.github.com>
2020-11-12 23:35:27 -0500

    stored assets on github

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 
-import Checkmark from '../assets/checkmark.svg';
+//import Checkmark from '../assets/checkmark.svg';
 
 export default class InputField extends Component {
 
     render() {
         return (
             <div className="inputField">
-                <label htmlFor={this.props.name}>{this.props.title}{this.props.filled && <img src={Checkmark} title={'Completed!'} alt={''}/>}</label>
+                <label htmlFor={this.props.name}>{this.props.title}{/*this.props.filled && <img src={Checkmark} title={'Completed!'} alt={''}/> */}</label>
                 <input 
                     type={this.props.name === 'email' ? 'email' : 'text'} 
                     name={this.props.name} 
