30bd8a5616d4078e410f761b763ee963b61f219e
Sang Nguyen <44916061+SangMNguyen@users.noreply.github.com>
2024-01-31 03:43:42 -0500

    Removed unused form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,33 +1,4 @@
-import React, {Component} from 'react';
-import InputField from '../components/InputField';
-
-const form = `<form className="emailForm" onSubmit={this.sendEmail}>
-    <InputField 
-        name="name" 
-        title="Name" 
-        placeholder="i.e. John Smith" 
-        onChange={this.handleChange} 
-        filled={this.state.name.length > 0}
-    />
-    <InputField 
-        name="email" 
-        title="Email Address" 
-        placeholder="i.e. johnsmith@gmail.com" 
-        onChange={this.handleChange}
-        filled={/^[a-zA-Z0-9.]+@[a-z]+\.(([a-z]{3})|([a-z]{2}\.[a-z]{2}))$/.test(this.state.email)}
-    />
-    <span className="inputField">
-        <label htmlFor="message">Message</label>
-        <textarea 
-            type="text" 
-            name="message" 
-            placeholder="Your projects are subpar, but you look nice enough." 
-            onChange={this.handleChange} 
-            className={this.state.message.length > 0 ? 'filled' : ''} 
-            required
-        />
-    </span>
-</form>`
+import React, { Component } from 'react';
 
 const email = require("../assets/email.png");
 const copied = require("../assets/copied.png");
